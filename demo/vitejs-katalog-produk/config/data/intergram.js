b4b77d7e260f859102e51bcd7812bd7bfae98d0f
Erik Sanjaya <eriiksanjaya@gmail.com>
2023-10-07 21:53:09 +0700

    sale

---
+++ demo/vitejs-katalog-produk/config/data/intergram.js
@@ -0,0 +1,16 @@
+const intergram = {
+  status: 1,
+  label: {
+    menu: "Chat",
+    title: "Chat",
+  },
+  id: "yourID",
+  mainColor: "#13283D",
+  titleClosed: "",
+  titleOpen: "Chat",
+  introMessage: "Halo, selamat datang, ada yang bisa kami bantu?",
+  autoResponse: "Mohon tunggu, sedang mencari admin yang sedang online.",
+  autoNoResponse:
+    "Sepertinya tidak ada yang bisa menjawab saat ini. Tolong beritahu kami bagaimana kami dapat menghubungi Anda, dan kami akan menghubungi Anda kembali sesegera mungkin.",
+  alwaysUseFloatingButton: true,
+};
