0215d72041333f33e851c0839c0eb088d3376d42
Erik Sanjaya <eriiksanjaya@gmail.com>
2025-04-28 19:33:19 +0700

    ok

---
+++ mikrotik-hotspot-qhbv-18/assets/js/term.js
@@ -0,0 +1,18 @@
+var cfgTerm = {
+    title: "Terms & Conditions of Purchase",
+    lists: [
+        "One-time purchase only, no monthly or yearly fees.",
+        "Supports Android 5 and above.",
+        "1 License for 1 IP or DNS Name for personal use. For another IP or DNS name, purchase again.",
+        "We only sell templates, we do not provide services (editing or uploading to MikroTik yourself).",
+        "An editing guide is provided, if there are issues editing, we will guide you.",
+        "Sorry, we don't understand MikroTik. We can answer questions about our template, but there's no guide for uploading to MikroTik. If you can't upload yourself, don't buy it.",
+        "Products that have been purchased and shipped cannot be exchanged or refunded.",
+        "Ensure the IP or DNS name entered is correct. No guarantees for errors in entering the IP or DNS name.",
+        "There is an Isolated Page, we only provide it, no settings can be made. Please read point 7 again.",
+        "Once purchased, questions should be about FEATURES only. For anything beyond that, sorry, we can't assist.",
+        "We disclaim responsibility if our product is used in violation of Islamic principles.",
+        "Any form of duplication, reproduction, or unauthorized use is illegal. We urge everyone to respect the creator's rights and maintain the creative integrity of this product. Violating our rules will have consequences in the Hereafter 😎.",
+        "Purchasing means you understand and agree to these terms.",
+    ]
+}
